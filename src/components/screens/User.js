a53ef58aa58c7c913b7ae81fb6f03d747bcce0f9
idaliya <idaliya.taisina@gmail.com>
2019-06-27 18:09:51 +0300

    logout

--- src/components/screens/User.js
+++ src/components/screens/User.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, View, Button } from 'react-native';
 
-export default class User extends Component {
+import { logoutUser } from '../../sagas/authSagas';
+
+class User extends Component {
   render() {
     return (
       <SafeAreaView>
-        <View />
+        <View>
+          <Button onPress={this.props.logoutUser} title="logoutUser" />
+        </View>
       </SafeAreaView>
     );
   }
 }
+
+export default connect(
+  undefined,
+  { logoutUser }
+)(User);
